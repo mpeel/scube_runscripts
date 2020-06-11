b9371dbb8367dce6d33a7007c6dc6b258da800ba
Mike Peel <email@mikepeel.net>
2020-06-11 15:48:34 +0100

    +

---
+++ perseus/runall.sh
@@ -0,0 +1,4 @@
+# K-band
+./scube_1910_version/SCUBE -exec scube_runscripts/perseus/import_kband.script
+./scube_1910_version/SCUBE -exec scube_runscripts/perseus/flag_kband.script
+./scube_1910_version/SCUBE -exec scube_runscripts/perseus/cal_mark_kband.script
